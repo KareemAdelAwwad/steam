5f17d22d4737008fd0a92427d4fe28f5ef563877
Kareem Adel <kareemadel10110@gmail.com>
2024-07-03 00:34:41 +0300

    Update Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Steam/',
   server: {
     port: 3000,
     historyApiFallback: true,
   },
+  
 })
