12179db3b339010675891a5244e25fd176b04eb6
Kareem Adel <kareemadel10110@gmail.com>
2024-07-02 23:54:40 +0300

    Update Vite configuration and add react-router-dom dependency

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  plugins: [react()],
   server: {
     port: 3000,
     historyApiFallback: true,
-  },
-  plugins: [react()],
-  base: './',
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
   },
 })
