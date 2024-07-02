4496c2c9443f774189b4dfa6b041280c7a9d25cd
Kareem Adel <kareemadel10110@gmail.com>
2024-07-02 12:47:15 +0300

    Update Vite configuration and fix import in MainLayout.jsx

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
-
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@components': path.resolve(__dirname, './src/components'),
-    },
-  },
   server: {
     port: 3000,
     historyApiFallback: true,
   },
   plugins: [react()],
-  base: '/',
+  base: './',
   build: {
     rollupOptions: {
       output: {
