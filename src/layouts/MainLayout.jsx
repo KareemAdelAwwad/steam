71154134dc68cca4bcbfdd847a4755ad12cecf45
Kareem Adel <kareemadel10110@gmail.com>
2024-07-03 00:57:15 +0300

    refactor: Update import paths in MainLayout.jsx

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,5 +1,5 @@
-import ClientNav from "../components/ClientNav";
-import ClientFooter from "../components/ClientFooter";
+import ClientNav from '../components/ClientNav';
+import ClientFooter from '../components/ClientFooter';
 import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
