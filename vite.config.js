16a8df85ae26002aad9e3beb5bf8f93871e253f1
Kareem Adel <kareemadel10110@gmail.com>
2024-07-02 12:10:44 +0300

    Remove unused files and update Vite configuration, Add deploy workflow.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
   },
   plugins: [react()],
   base: './',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  // Add this section for handling client-side routing
+  server: {
+    historyApiFallback: true,
+  },
 })
