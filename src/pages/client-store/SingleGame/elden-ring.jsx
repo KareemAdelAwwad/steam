4c8015e48498e5f06e026c76764e32535427064e
Kareem Adel <kareemadel10110@gmail.com>
2024-10-03 10:59:02 +0300

    fixes

--- src/pages/client-store/SingleGame/elden-ring.jsx
+++ src/pages/client-store/SingleGame/elden-ring.jsx
@@ -56,7 +56,7 @@ const EldenRing = () => {
     <div className="w-full h-full bg-secondary m-0 py-8">
       <section className="container 2xl:w-[1567px] mx-auto max-sm:px-4 flex flex-col  gap-2.5">
         <header className="w-full h-fit p-4 bg-bg-main flex lg:flex-row flex-col
-        justify-evenly items-center rounded overflow-hidden">
+        justify-between items-center rounded overflow-hidden">
           <h2 className="text-white text-4xl heading-large ml-2">
             {game.name}
           </h2>
@@ -168,21 +168,12 @@ const EldenRing = () => {
                   className="w-full bg-bg-main bg-opacity-50 p-4 rounded flex flex-col gap-2.5 "
                   key={index}
                 >
-<<<<<<< HEAD
                   <div className="flex">
                     <div className="flex justify-between items-center w-[70%]">
                       <h2 className="heading-medium">{edition.name}</h2>
                       <Platform game={game} size={25} />
                     </div>
                     <div className="flex justify-end items-center gap-3 w-[30%]">
-=======
-                  <div className="flex sm:flex-row flex-col justify-between">
-                    <div className="flex justify-between items-center lg:w-[40%] w-full">
-                      <h2 className="heading-medium">{edition.name}</h2>
-                      <Platform game={game} size={25} />
-                    </div>
-                    <div className="flex justify-end items-center gap-3 w-full lg:w-[30%]">
->>>>>>> ad8c524b070e5f544d2858d2c4d755b6d7ead570
                       <Discount game={edition} />
                       <AddToCart title="Add to cart" />
                     </div>
